387bdb758b960e51a98b6b4e097385c7d6d06788
alpibeny <alpibeny@gmail.com>
2026-09-20 12:16:50 +0300

    2

---
+++ app/src/main/java/by/killraider/app/Screen.kt
@@ -0,0 +1,44 @@
+package by.killraider.app
+
+import androidx.compose.ui.graphics.Color
+
+sealed class Screen ( val route: String,
+                      val title: String,
+                      val iconId: Int,
+                      val activeIconId: Int,
+                      val backgroundColor: Color) {
+
+    object Home : Screen(
+        route = "home",
+        title = "Главная",
+        iconId = R.drawable.menu_home,
+        activeIconId = R.drawable.menu_home_active,
+        backgroundColor = Color(0xFF0F172A)
+    )
+
+    object Workout : Screen(
+        route = "workout",
+        title = "Тренировки",
+        iconId = R.drawable.menu_train,
+        activeIconId = R.drawable.menu_train_active,
+        backgroundColor = Color(0xFF1E1B4B)
+    )
+
+    object Book : Screen (
+        route = "book",
+        title = "Книги",
+        iconId = R.drawable.menu_book,
+        activeIconId = R.drawable.menu_book_active,
+        backgroundColor = Color(0xFF064E3B)
+    )
+
+    object Stats : Screen(
+        route = "stats",
+        title = "Статистика",
+        iconId = R.drawable.menu_rank,
+        activeIconId = R.drawable.menu_book_active,
+        backgroundColor = Color(0xFF581C87)
+    )
+
+
+}
